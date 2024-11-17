d88647263af7231080974655e611417ea21e9096
Eduardo Moyla <eduardomoyla@gmail.com>
2024-11-17 18:38:47 -0400

    cart changes

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -13,8 +13,16 @@ function CartProvider ({children}) {
         return total
     }
 
+    const clearCart = () => setCart([])
+
+    const getTotal = () => {
+        const priceOnly = cart.map(item => item.price*item.qty)
+        const total = priceOnly.reduce((acc, current) => acc + current, 0)
+        return total
+    }
+
     return (
-        <cartContext.Provider value={{cart, addToCart, totalQty}}> 
+        <cartContext.Provider value={{cart, addToCart, totalQty, getTotal, clearCart}}> 
             {children}
         </cartContext.Provider>
     )
